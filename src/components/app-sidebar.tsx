69a9561b06d5e458408c13f12c32646e7e334c43
Nicolas Perez <nbome12@outlook.com>
2025-04-25 17:07:11 -0500

    Cambio en el nombre de la barside

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -114,11 +114,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <div className="bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium text-xl">Mobius</span>
+                  <span className="truncate font-medium text-xl">Noit</span>
                 </div>
               </a>
             </SidebarMenuButton>
