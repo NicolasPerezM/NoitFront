2f9e28eadf6060677c09fdb5fa29cd9752ba30a5
Nicolas Perez <nbome12@outlook.com>
2025-05-03 07:12:36 -0500

    cambios en el login

--- src/features/auth/components/ParticlesBackground.tsx
+++ src/features/auth/components/ParticlesBackground.tsx
@@ -24,7 +24,7 @@ export default function ParticlesBackground() {
             color: { value: "#ffffff" },
             links: {
               color: "#ffffff",
-              distance: 150,
+              distance: 180,
               enable: true,
               opacity: 0.5,
               width: 1,
