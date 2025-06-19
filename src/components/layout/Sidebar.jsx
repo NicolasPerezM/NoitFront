5c8f4523b54f42ac1122d339eadd934bf053b302
Nicolas Perez <nbome12@outlook.com>
2025-06-19 18:09:31 -0500

    cambios en layout

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -4,13 +4,15 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 export default function Page({children}) {
   return (
-    <div className="[--header-height:calc(theme(spacing.14))]">
-      <SidebarProvider className="flex flex-col">
+    <div className="[--header-height:calc(theme(spacing.14))] bg-theme-white dark:bg-theme-darkest text-theme-darkest dark:text-theme-light font-montreal text-paragraph">
+      <SidebarProvider className="flex flex-col border">
         <SiteHeader />
         <div className="flex flex-1">
           <AppSidebar />
           <SidebarInset>
-          <div className="p-4">{children}</div>
+            <div className="w-auto">
+              {children}
+            </div>
           </SidebarInset>
         </div>
       </SidebarProvider>
